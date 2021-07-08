67bcef1d9364eb7dd5d469d567106975de9a6a53
JessePecar <JPecar19@gmail.com>
2021-07-08 12:21:02 -0400

    added another car component for testing and flushed out the car component before splitting it into more sub components

--- formula-one-history/tailwind.config.js
+++ formula-one-history/tailwind.config.js
@@ -2,10 +2,16 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: 'media',
   theme: {
-    extend: {},
+    extend: {
+      blur: {
+        xs: '1px'
+      }
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      blur: ['hover', 'focus']
+    },
   },
   plugins: []
 }
